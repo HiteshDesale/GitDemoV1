9a12c2822693aa7a2f8f0f65691fa292b65d2767
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-05 16:08:15 +0530

    Pushing HiteshQAEngineer written code

---
+++ src/demo1/sample2.java
@@ -0,0 +1,27 @@
+package demo1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sample2 
+{
+	//@Author by HiteshDesale team lead
+	public static void main(String[] args)
+	{
+		String sName="Rahul";
+
+		if (sName == "Hitesh")
+		{
+			System.out.println("The student name is correct");
+		}
+		else
+		{
+			System.out.println("The student name is incorrect");
+		}
+
+
+
+
+	}
+
+}

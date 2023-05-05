324336d7c3394652988be81b3065aa5f59eee67c
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-05 16:09:11 +0530

    Pushing team lead written script

--- src/demo1/sample1.java
+++ src/demo1/sample1.java
@@ -5,20 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class sample1 
 {
+	//@Author by HiteshDesale team lead
 	public static void main(String[] args)
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hitesh\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://en-gb.facebook.com/");
-		
-//		System.out.println("hello who are you");
-		
-		
-		
-		
-		
+		String sName="Hitesh";
+
+		if (sName == "Hitesh")
+		{
+			System.out.println("The student name is correct");
+		}
+		else
+		{
+			System.out.println("The student name is incorrect");
+		}
+
+
+
+
 	}
 
 }
